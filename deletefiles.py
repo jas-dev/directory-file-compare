b1fcb62e8838607eda7ddfaa798d1a67b35580c7
jas-dev <jaysamora@outlook.com>
2024-01-29 06:55:57 -0700

    created file processing function to handle deletion

--- deletefiles.py
+++ deletefiles.py
@@ -5,6 +5,22 @@ def delete_file(file_path):
     try:
         os.remove(file_path)
         print(f"Deleted: {file_path}")
+
     except OSError as e:
         print(f"Error: {e.strerror}. File: {file_path}")
-        
+
+def process_file(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            for line in file:
+
+                # Handle both lineseparated and comma-separated values
+                file_paths = line.strip().split(',')
+                for path in file_paths:
+                    if path: # check path is not empty
+                        delete_file(path.strip())
+
+    except FileNotFoundError:
+        print(f'File not found: {file_name}')
+    except Exception as e:
+        print(f'Error: {e}')
